0fd4fd2fd8f8245eed729258bd93083643afed58
alla-andronova <allaitcreative@gmail.com>
2021-10-13 17:38:11 +0300

    add hm04

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,11 @@
 const bcrypt = require('bcryptjs');
 const { Conflict, NotFound, BadRequest } = require('http-errors');
+const dotenv = require('dotenv');
+dotenv.config();
 const { User } = require('../models');
 const { userSchema } = require('../schemas');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = process.env;
 
 const signup = async (req, res, next) => {
   try {
@@ -46,6 +50,12 @@ const signup = async (req, res, next) => {
 
 const login = async (req, res, next) => {
   try {
+    const { error } = userSchema.validate(req.body);
+
+    if (error) {
+      const err = new BadRequest(error.message);
+      throw err;
+    }
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
@@ -54,19 +64,68 @@ const login = async (req, res, next) => {
     if (!bcrypt.compareSync(password, user.password)) {
       throw new BadRequest('failed');
     }
+    const payload = {
+      id: user.id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY);
+    await User.findByIdAndUpdate(user.id, { token });
+
+    res.json({
+      status: 'success',
+      token,
+      user: {
+        email,
+        subscription: 'starter',
+      },
+    });
   } catch (error) {
     next(error);
   }
 };
-// const logout = async (req, res, next) => {
-//   try {
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+const logout = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const user = await User.findByIdAndUpdate(id, { token: null });
+    if (!user.token) {
+      res.status(401).json({
+        status: 'error',
+        message: 'not authorized',
+        code: 401,
+      });
+    }
+    res.status(204).json({
+      status: 'success deleted',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getUserData = async (req, res, next) => {
+  try {
+    const { id, email, subscription } = req.user;
+    const user = await User.findById(id);
+    if (!user.token) {
+      res.status(401).json({
+        status: 'error',
+        message: 'not authorized',
+        code: 401,
+      });
+    }
+    res.status(200).json({
+      ResponseBody: {
+        email,
+        subscription,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   signup,
   login,
-  // logout,
+  logout,
+  getUserData,
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const authMiddleware = require('../../middleware/auth');
 const { users: controller } = require('../../controllers');
 module.exports = router;
 
 router.post('/signup', controller.signup);
-// router.post('./login', controller.login);
-// router.get('./logout', controller.logout);
+router.post('/login', controller.login);
+router.get('/logout', authMiddleware, controller.logout);
+router.get('/current', authMiddleware, controller.getUserData);
